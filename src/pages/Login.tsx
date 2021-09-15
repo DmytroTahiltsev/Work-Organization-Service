c9a31bcb9eee6810efcfc3c0fddb1404649fe26b
DmytroTahiltsev <dima.t16042001@gmail.com>
2021-09-15 21:50:53 +0300

    login done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,16 @@
+import { Layout, Row, Card } from "antd";
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 const Login: React.FC = () => {
     return(
-        <div>
-            Login
-        </div>
+        <Layout>
+            <Row justify="center" align="middle" className="h100">
+                <Card>
+                    <LoginForm />
+                </Card>
+            </Row>
+        </Layout>
     )
 }
 export default Login
